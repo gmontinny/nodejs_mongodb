720763fc54bd3fef9a54d913a8ce49420e2afcc4
Giovanny <gmontinny@hotmail.com>
2022-05-14 09:50:54 -0400

    Alteraćao no controller livro

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -2,14 +2,18 @@ import livros from "../models/Livro.js";
 
 class LivroController{
     static listarLivros = (req, res) => {
-        livros.find((err,livros) => {
+        livros.find()
+            .populate('autor')
+            .exec((err,livros) => {
             res.status(200).json(livros);
         });
     }
 
     static listarLivroPorId = (req, res) => {
         const id = req.params.id;
-        livros.findById(id, (err,livros) => {
+        livros.findById(id)
+            .populate('autor','nome')
+            .exec((err,livros) => {
            if(err){
                res.status(400).send({message: `${err.message} - livro não encontrado.`});
            }else {
@@ -50,6 +54,13 @@ class LivroController{
                res.status(500).send({message: `${err.message} - falha ao excluir livro.`});
            }
         });
+    }
+
+    static listarLivroPorEditora = (req, res) => {
+        const editora = req.query.editora;
+        livros.find({'editora':editora},{}, (err,livros) => {
+            res.status(200).send(livros);
+        })
     }
 }
 
